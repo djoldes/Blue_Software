dc97e7b1bdf8cd3557533ef54f8e0966a82508f3
djoldes <davejoldes@gmail.com>
2023-04-05 17:33:08 +0300

    Connected db to project

--- Blue_Software/SignUp.cs
+++ Blue_Software/SignUp.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.SqlClient;
 
 namespace Blue_Software
 {
     public partial class SignUp : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=Users_Blue;Integrated Security=True");
         public SignUp()
         {
             InitializeComponent();
@@ -20,8 +22,15 @@ namespace Blue_Software
 
         private void button1_Click(object sender, EventArgs e)
         {
-            new Form1().Show();
-            this.Hide();
+            /*new Form1().Show();
+            this.Hide();*/
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into SignUp_Blue values('" + txtFirstName + "','" + txtLastName + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data added succesfully");
         }
 
         private void button2_Click(object sender, EventArgs e)
