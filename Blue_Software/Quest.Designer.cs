5e9afb8a803b19e8d98bdb55bda0a1ca124f66f7
djoldes <davejoldes@gmail.com>
2023-04-09 17:06:54 +0300

    Updated the credits when the answer is correct(the same with the one in db)

--- Blue_Software/Quest.Designer.cs
+++ Blue_Software/Quest.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textAnswer = new System.Windows.Forms.TextBox();
+            this.btnAnswer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +42,30 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
+            // textAnswer
+            // 
+            this.textAnswer.Location = new System.Drawing.Point(28, 135);
+            this.textAnswer.Name = "textAnswer";
+            this.textAnswer.Size = new System.Drawing.Size(125, 27);
+            this.textAnswer.TabIndex = 1;
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(28, 185);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(139, 29);
+            this.btnAnswer.TabIndex = 2;
+            this.btnAnswer.Text = "Send Answer";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
             // Quest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.textAnswer);
             this.Controls.Add(this.label1);
             this.Name = "Quest";
             this.Text = "Quest";
@@ -56,5 +77,7 @@
         #endregion
 
         private Label label1;
+        private TextBox textAnswer;
+        private Button btnAnswer;
     }
 }
--- Blue_Software/Quest.cs
+++ Blue_Software/Quest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,27 @@ namespace Blue_Software
             InitializeComponent();
             label1.Text = text;
         }
-        
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            int credit = AppData.CurrentUser.Credit;
+            string connString = @"Data Source =.; Initial Catalog = Users_Blue; Integrated Security = True";
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Texte WHERE Answer = @text", conn);
+            cmd.Parameters.AddWithValue("@text", textAnswer.Text);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                MessageBox.Show("The answer is correct! You won 50 credits!");
+                AppData.CurrentUser.UpdateCredit(credit += 50);
+            }
+            else
+            {
+                MessageBox.Show("The answer is wrong!");
+            }
+            reader.Close();
+            conn.Close();
+        }
     }
 }
